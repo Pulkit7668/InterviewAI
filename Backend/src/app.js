c6ca572212d510b8c24b5d97f650f974d85d174a
gagan gautam <gagangautam@gagans-MacBook-Air.local>
2026-05-17 19:41:13 +0530

    cors fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,16 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
+app.use(cors([
+    {
+        origin: "https://interview-ai-flame-two.vercel.app",
+        credentials: true
+    },
+    {
     origin: "http://localhost:5173",
     credentials: true
-}))
+    }
+]))
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
